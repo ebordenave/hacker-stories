7fa0a3fcdbfa51759c16dd62d6b9ac05c43d952b
Edward Bordenave <edward.bordenave@gmail.com>
2023-11-28 11:33:05 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import {useState} from "react";
 
 
 const App = () => {
+  // Initialize stories array, containing objects
+  // These Objects will represent each story and have properties associated
+  // to them
   const stories = [
     {
       title: 'React',
@@ -23,23 +26,32 @@ const App = () => {
     }
   ]
 
+  // useState is used to set a state value. We get (getter) a value
+  // we then set a value (setter). We use Destructuring here.
   const [searchTerm, setSearchTerm] = useState('')
-  console.log(searchTerm)
+  // console.log to see the value we've set in the console
+  // console.log(searchTerm)
 
+  // handler function: This handler will "handle" the search behavior
+  // meaning we pass in an event as our argument, and set the searchTerm to be
+  // the target's value. event.target.value. This allows the user to input a
+  // search term and will become the state value when searching
   const handleSearch = (e) => {
     setSearchTerm(e.target.value)
-    // console.log(e.target.value)
   }
 
+
   const searchedStories = stories.filter(function (story) {
-    return story.title.toLowerCase().includes(searchTerm.toLowerCase())
+    return story.title.toLowerCase()
+      .includes(searchTerm.toLowerCase())
   })
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <Search search={searchTerm} onSearch={handleSearch}/>
-      <hr />
+      <Search search={searchTerm}
+              onSearch={handleSearch}/>
+      <hr/>
       <List list={searchedStories}/>
     </div>
 
@@ -51,30 +63,43 @@ export default App
 const List = ({list}) => (
   <ul>
     {list.map((item) => (
-      <Item key={item.objectID} item={item}/>
+      <Item key={item.objectID}
+            title={item.title}
+            item={item}/>
     ))}
   </ul>
 )
 
-const Item = (props) => {
-  return (
-    <li>
+const Item = ({
+                item: {
+                  title,
+                  url,
+                  author,
+                  num_comments,
+                  points,
+                },
+              }) => (
+  <li>
       <span>
-        <a href={props.item.url}>{props.item.title}</a>
+        <a href={url}>{title}</a>
       </span>
-      <span> {props.item.author}</span>
-      <span> {props.item.num_comments}</span>
-      <span> {props.item.points}</span>
-      {/* <span> {props.item.url}</span> */}
-    </li>
-  )
-}
+    <span> {author}</span>
+    <span> {num_comments}</span>
+    <span> {points}</span>
+    {/* <span> {props.item.url}</span> */}
+  </li>
+)
 
-const Search = ({onSearch}) => {
+const Search = ({search, onSearch}) => {
 
   return (<div>
       <label htmlFor="search">Search: </label>
-      <input type="text" id="search" onChange={onSearch}/>
+      <input
+        id="search"
+        type="text"
+        value={search}
+        onChange={onSearch}
+      />
     </div>
   )
 }
